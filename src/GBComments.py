5b3da477162eff36fdc29e929a77d478334f6b2d
WRAllen <1072274105@qq.com>
2020-01-06 14:24:11 +0800

    修改错误的导包

--- src/GBComments.py
+++ src/GBComments.py
@@ -1,10 +1,6 @@
-import json
 import random
 
-def readJson(filename):
-    with open(filename, mode='r',encoding="utf-8") as file:
-        return json.loads(file.read())
-
+from data import data
 
 def produceComments(_type="好评", _class="鞋子", filename="data.json"):
     """
@@ -12,12 +8,11 @@ def produceComments(_type="好评", _class="鞋子", filename="data.json"):
     :param _class: 商品类目，例如：鞋子
     """
     try:
-        data = readJson(filename)
-        comments = data[_type]
+        comments = data.datas[_type]
         return (random.choice(comments["开始"]) + random.choice(comments["中间"]) + random.choice(comments["结束"])).replace("$", _class)
     except Exception as e:
         print("{} 文件找不到，请放在main的同目录下！".format(e))
     
 
 if __name__ == "__main__":
-    produceComments(_type="好评", _class="手机")
+    print(produceComments(_type="好评", _class="手机"))
