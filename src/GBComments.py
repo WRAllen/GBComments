4edd8819cca5fe840fdb7fd444d3a30d381761cf
WRAllen <1072274105@qq.com>
2020-01-06 16:48:16 +0800

    修改setup.py支持markdown

--- src/GBComments.py
+++ src/GBComments.py
@@ -2,7 +2,7 @@ import random
 
 from data import data
 
-def produceComments(_type="好评", _class="鞋子", filename="data.json"):
+def produceComments(_type="好评", _class="鞋子"):
     """
     :param _type: 好评 或者 差评
     :param _class: 商品类目，例如：鞋子
@@ -11,7 +11,7 @@ def produceComments(_type="好评", _class="鞋子", filename="data.json"):
         comments = data.datas[_type]
         return (random.choice(comments["开始"]) + random.choice(comments["中间"]) + random.choice(comments["结束"])).replace("$", _class)
     except Exception as e:
-        print("{} 文件找不到，请放在main的同目录下！".format(e))
+        print("{} 文件找不到，请检查目录下面的data文件！".format(e))
     
 
 if __name__ == "__main__":
