28e7b72e9f9393ac456b23d5b52556e0afd132c3
WRAllen <1072274105@qq.com>
2020-01-06 11:06:13 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
     name='GBComments',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['GBComments'],
     author="WRAllen",
     author_email="1072274105@qq.com",
